b4d674b313a7c2d111b3dc3da225a930ab0fbee8
elameensw <elameensw@gmail.com>
2023-11-30 08:05:22 -0800

    My commit

---
+++ python-functions/6-password.py
@@ -0,0 +1,23 @@
+def validate_password(password):
+    if (len(password)) < 8:
+        return False
+
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+
+        if has_uppercase and has_lowercase and has_digit:
+            break
+
+    if ' ' in password:
+        return False
+
+    return has_uppercase and has_lowercase and has_digit
