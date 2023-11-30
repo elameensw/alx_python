259a368d71f3d348075dae758f59a8419140af31
elameensw <elameensw@gmail.com>
2023-11-30 08:17:42 -0800

    My commit

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,11 +1,8 @@
 def is_prime(number):
-    if number < 2:
+    if number <= 1:
         return False
-    elif number == 2:
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                return False
         return True
-
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            return False
-
-    return True
