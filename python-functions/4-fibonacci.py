962695c753873f55ad53abf604f7620aff516dbc
elameensw <elameensw@gmail.com>
2023-11-30 08:15:59 -0800

    My commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,13 +1,6 @@
 def fibonacci_sequence(n):
-    if n == 0:
-        return 0
-    else if n == 1:
-        return 1
-
-    prev_num, current_num = 0, 1
-    for n in range(2, n+1):
-        next_num = prev_num  + current_num
-        prev_num, current_num = current_num, next_num
-
-    return current_num
-    
+    fibunacci = [0, 1]
+    while len(fibunacci) < n:
+            anothernum = fibunacci[-1] + fibunacci[-2]
+            fibunacci = fibunacci + [anothernum]
+    return fibunacci[:n]
