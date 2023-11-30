4b8cf9255c2c1c3ccb3f6db53919b8694d21c695
elameensw <elameensw@gmail.com>
2023-11-30 08:01:30 -0800

    My commit

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,6 +1,5 @@
-'''
-Write a Python function called reverse_string that takes a string as input and returns the reverse of that string.
-'''
 def reverse_string(string):
-    return string[::-1]
-print(reverse_string("hello"))
+    reversed_string = ""
+    for char in string:
+        reversed_string = char + reversed_string
+    return reversed_string
